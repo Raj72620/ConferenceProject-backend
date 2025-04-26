fae80fd5a57b817171b6e72ab54a5ce1f789bc8b
Raj Nishanth <nishanthraj9618@gmail.com>
2025-04-26 15:50:04 +0530

    Updated backend files with recent changes

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 });
 
 // ==================== Routes ====================
-app.use("/api/register", registrationRoutes.router);
+app.use("/api/register", registrationRoutes.route);
 app.use("/api/contact", contactRoutes);
 app.use("/submit", paperRoutes);
 
