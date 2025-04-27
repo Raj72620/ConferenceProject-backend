249385cda0515667e47445bb2ed3d6354b2768a7
Raj Nishanth <nishanthraj9618@gmail.com>
2025-04-27 15:17:05 +0530

    Updated backend files with recent changes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,5 +3,5 @@ const express = require("express");
 const router = express.Router();
 const { submitContact } = require("../controllers/contactController");
 
-router.post("/contact", submitContact);
+router.post("/api/contact", submitContact);
 module.exports = router; 
